c2289452da6e000ca6a7a3c86c15c0fdc5115889
Lachlanjowett <79903111+lochyj@users.noreply.github.com>
2022-10-17 08:34:16 +1100

    Added error handling and final commit

--- index.js
+++ index.js
@@ -11,21 +11,26 @@ app.get('/', function(req, res) {
     res.json({
         message: 'Hello World!'
     });
-});
+}).on('error', function(error) {handle_error(error, 'http.get');});
 
 app.post('/post', function(req, res) {
     console.log(req.body);
     res.send('ok');
-});
+}).on('error', function(error) {handle_error(error, 'http.post');});
 
 app.put('/put', function(req, res) {
     console.log(req.body);
     res.send('ok');
-});
+}).on('error', function(error) {handle_error(error, 'http.put');});
 
 
 http.listen(port, function() {
     console.log('listening on *:' + port);
-}).on('error', function(err) {
-    console.log(err);
-});
+}).on('error', function(error) {handle_error(error, `http.server init for port ${port}`);});
+
+function handle_error(error, location) {
+    console.log(`
+        | ${error} |
+        | ${location} |
+    `)
+}
